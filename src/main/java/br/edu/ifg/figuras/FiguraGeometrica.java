41e8e3e4f7a172310c67e67a0d5946991155379a
victorbento <vgb.info@gmail.com>
2016-02-24 21:35:01 -0300

    Primeiro comit maven

---
+++ src/main/java/br/edu/ifg/figuras/FiguraGeometrica.java
@@ -0,0 +1,10 @@
+package br.edu.ifg.figuras;
+
+/**
+ *
+ * @author Victor
+ */
+public abstract class FiguraGeometrica {
+    public abstract Long getArea();
+    public abstract Long getPerimetro();
+}
