5bab28c366577428e6266c2802ae3bc7b7bc0697
victorbento <vgb.info@gmail.com>
2016-02-24 21:59:32 -0300

    Circulo

---
+++ src/main/java/br/edu/ifg/figuras/Circulo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifg.figuras;
+
+/**
+ *
+ * @author Victor
+ */
+public class Circulo extends FiguraGeometrica{
+    private Long raio;
+    
+    public Circulo(Long raio){
+        this.raio = raio;
+    }
+    
+    @Override
+    public Long getArea() {
+       return (long) Math.PI * (long) Math.pow(raio, 2);
+    }
+
+    
+    @Override
+    public Long getPerimetro() {
+        return (2 * ((long) Math.PI))  * raio; 
+    }
+    
+}
