87137f39bf57a2c1fdca1ba0d00a4afdb68d3cbf
admin <admin@S403C-02>
2016-02-25 21:57:43 -0300

    Test do Retangulo

---
+++ src/test/java/br/edu/ifg/figuras/RetanguloTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifg.figuras;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author admin
+ */
+public class RetanguloTest {
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void deveCalcularAreaCorretamente(){
+        Retangulo retangulo = new Retangulo(4L,8L);
+        assertEquals(31L,(long) retangulo.getArea());
+        
+        
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
